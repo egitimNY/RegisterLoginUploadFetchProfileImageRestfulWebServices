25e1c510a1a2fad634253ac25b9ee90527522dbf
indirjavam <indirjava@gmail.com>
2020-12-06 13:45:56 -0500

    Created getUserDetail(); Method & getting Users detail in profile and created menu_action.xml

--- app/src/main/java/com/ozkayahalit/HomeActivity.java
+++ app/src/main/java/com/ozkayahalit/HomeActivity.java
@@ -1,19 +1,38 @@
 package com.ozkayahalit;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.HashMap;
+import java.util.Map;
 
 public class HomeActivity extends AppCompatActivity {
 
+    private static final String TAG = HomeActivity.class.getSimpleName(); // getting the info
     private TextView name, email;
     private Button btn_logout, btn_photo_upload;
     SessionManager sessionManager;
+    String getId;
+    private static String URL_READ = "https://ozkayahalit.com/read_detail.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,13 +47,7 @@ public class HomeActivity extends AppCompatActivity {
         btn_logout = findViewById(R.id.btn_logout);
 
         HashMap<String, String> user = sessionManager.getUserDetail();
-        String mName = user.get(sessionManager.NAME);
-        String mEmail = user.get(sessionManager.EMAIL);
-
-        name.setText(mName);
-        email.setText(mEmail);
-
-
+        getId = user.get(sessionManager.ID);
 
         btn_logout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,5 +56,74 @@ public class HomeActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    private void getUserDetail(){
+
+        final ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Loading...");
+        progressDialog.show();
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL_READ,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        progressDialog.dismiss();
+                        Log.i(TAG, response.toString());
+
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            String success = jsonObject.getString("success");
+                            JSONArray jsonArray = jsonObject.getJSONArray("read");
+
+                            if (success.equals("1")){
+
+                                for (int i =0; i < jsonArray.length(); i++){
+
+                                    JSONObject object = jsonArray.getJSONObject(i);
+
+                                    String strName = object.getString("name".trim());
+                                    String strEmail = object.getString("email".trim());
+
+                                    name.setText(strName);
+                                    email.setText(strEmail);
+
+                                }
+
+                            }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            progressDialog.dismiss();
+                            Toast.makeText(HomeActivity.this, "Error Reading Detail "+e, Toast.LENGTH_LONG).show();
+                        }
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        progressDialog.dismiss();
+                        Toast.makeText(HomeActivity.this, "Error Reading Detail "+error, Toast.LENGTH_LONG).show();
+
+                    }
+                })
+        {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String > params = new HashMap<>();
+                params.put("id", getId);
+                return params;
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        getUserDetail();
     }
 }
